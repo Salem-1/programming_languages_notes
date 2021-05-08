f9840e0de452679b3481db9ff3fac99c50eefde3
Ahmed.Salem <123456>
2021-05-08 16:59:19 +0400

    adding all files first time

---
+++ git_commands.py
@@ -0,0 +1,13 @@
+#in the name of Allah the most merceful
+#in the terminal
+git clone <git url>
+# download the reprository from git-hub to my local machine
+#in the terminal cd to the reprository
+
+git add <filename>
+#add a file to the reprosiotory
+
+git commit -m "message"
+#save a snapshot of the changes I made to my file
+
+git push
