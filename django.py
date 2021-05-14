058e737aed758681cec114856dd580cd21c532e0
Ahmed.Salem <123456>
2021-05-14 23:30:14 +0400

    submitting valid form

--- django.py
+++ django.py
@@ -172,3 +172,56 @@ urlpatterns = [
 #after you are done with the page content
 #write this line of code
 {% endblock %}
+######################################taking form inputs######################
+
+#csrf = cross site request frogery
+# for example
+
+{% extends "tasks/layout.html" %}
+{% block body %}
+    <h1>Add task</h1>
+    <form action="{% url 'tasks:add' %}" method ="post"> # action is where do I want those inputs to go , method (get or post )
+      <input type="text" name="task" >
+      <input type="submit" >
+      </form>
+      <a href="{% url 'tasks:index' %}">view your tasks</a>
+  </body>
+</html>
+{% endblock %}
+###################################generating forms in django###################
+# in the views file
+from django import forms
+from django.shortcuts import render
+
+tasks = ["foo", "bar", "baz"]
+# Create your views here.
+
+#class for genrating forms, i believe in larger projects it should be in a separate module(file)
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task") #character field input & the label is what going to appear on top of the empty field
+    priority = forms.IntegerField (label="Priority", min_value=1, max_value=10)
+
+def index(request):
+    return render(request, "tasks/index.html",
+    {
+        "tasks": tasks
+    })
+
+def add(request):
+    if request.method == "POST": # check if the user submitted the form
+        form = NewTaskForm(request.POST)  #saving the submitted form in form variable
+
+        # checking if the submitted data is valid(built  in function in django)
+        if form.is_valid():
+            task = form.cleaned_data["task"] # saving the inputted task in task variable
+            priority = form.cleaned_data["priority"]
+            tasks.append(task) # adding the inputted task to our list
+        else:
+            return render(request, "tasks/add.html", {
+            "form": form
+            })
+
+    return render(request, "tasks/add.html",
+    {
+    "form":NewTaskForm()
+    })
--- edgedrone/tasks/views.py
+++ edgedrone/tasks/views.py
@@ -1,7 +1,13 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
 # Create your views here.
+
+#class for genrating forms, i believe in larger projects it should be in a separate module(file)
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task") #character field input & the label is what going to appear on top of the empty field
+    priority = forms.IntegerField (label="Priority", min_value=1, max_value=10)
 
 def index(request):
     return render(request, "tasks/index.html",
@@ -10,6 +16,20 @@ def index(request):
     })
 
 def add(request):
-    #new = item.get.something("newtask")
-    #tasks.append(new)
-    return render(request, "tasks/add.html")
+    if request.method == "POST": # check if the user submitted the form
+        form = NewTaskForm(request.POST)  #saving the submitted form in form variable
+
+        # checking if the submitted data is valid(built  in function in django)
+        if form.is_valid():
+            task = form.cleaned_data["task"] # saving the inputted task in task variable
+            priority = form.cleaned_data["priority"]
+            tasks.append(task) # adding the inputted task to our list
+        else:
+            return render(request, "tasks/add.html", {
+            "form": form
+            })
+
+    return render(request, "tasks/add.html",
+    {
+    "form":NewTaskForm()
+    })
