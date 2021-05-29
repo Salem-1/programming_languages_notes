f49eaf5a292a9ff1dc1aa6cdb6be134568f4084d
Ahmed.Salem <123456>
2021-05-29 16:18:49 +0400

    scalability and security

---
+++ scalabilitry-security/scalability-security-notes.py
@@ -0,0 +1,4 @@
+cashe-control: max-age=86400  # max number of seconds to store the case
+#unless those number of seconds has elapsed we are going to use the existing version
+#save the content in the browser cashed :)
+#natural to have the css file cashed  in th ebrowser itself
