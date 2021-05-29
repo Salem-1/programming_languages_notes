3ee2445218699b55b45b5e38bfeac86578828100
Ahmed.Salem <123456>
2021-05-29 12:11:01 +0400

    failing the airline app test

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -20,7 +20,7 @@ class Flights(models.Model):
         return f"{self.id}: {self.origin} to {self.destination} in {self.duration}"
 
     def is_valid_filght(self):
-        return self.origin != self.destination or self.duration >= 0
+    #    return self.origin != self.destination or self.duration >= 0
 
 class Passenger(models.Model):
     first = models.CharField(max_length=66)
